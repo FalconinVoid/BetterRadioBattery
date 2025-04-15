322a0cd0cb9b210be642f896afa6e01601a58f8f
FalconinVoid <141413671+FalconinVoid@users.noreply.github.com>
2025-04-15 20:00:32 +0200

    Update 1.1.0

--- BetterRadioBattery/Config.cs
+++ BetterRadioBattery/Config.cs
@@ -1,5 +1,6 @@
-﻿using Exiled.API.Interfaces;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+
+using Exiled.API.Interfaces;
 
 namespace BetterRadioBattery
 {
@@ -9,5 +10,10 @@ namespace BetterRadioBattery
         public bool Debug { get; set; } = false;
         [Description("Lower values mean lower drain!")]
         public float DrainMultiplier { get; set; } = 1f;
+        [Description("Additional Multipliers")]
+        public float ShortRangeMultiplier { get; set; } = 1f;
+        public float MiddleRangeMultiplier { get; set; } = 1f;
+        public float LongRangeMultiplier { get; set; } = 1f;
+        public float UltraRangeMultiplier { get; set; } = 1f;
     }
 }
